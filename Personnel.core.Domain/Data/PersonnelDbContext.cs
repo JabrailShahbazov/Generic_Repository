486c68df451670b7b0863930c94ddbdf57e728dd
Cebrail Shahbazov <cebrail.shahbazov@gmail.com>
2020-10-09 23:40:40 +0400

    Add project files.

---
+++ Personnel.core.Domain/Data/PersonnelDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Personnel.core.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Personnel.core.Domain.Models.Domain;
+
+namespace Personnel.core.Domain.Data
+{
+    public class PersonnelDbContext:DbContext
+    {
+        private readonly DbContextOptions<PersonnelDbContext> _options;
+
+        public PersonnelDbContext(DbContextOptions<PersonnelDbContext> options) : base(options)
+        {
+            _options = options;
+        }
+
+
+        public DbSet<Personnels>Personnels { get; set; }
+        public DbSet<Department>Departments { get; set; }
+
+
+
+
+    }
+}
